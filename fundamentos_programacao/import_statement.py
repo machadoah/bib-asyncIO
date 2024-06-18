7f6c73ae4697a0cf0692d9af0869f3cb4342860b
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-06-17 22:30:03 -0300

    status

---
+++ fundamentos_programacao/import_statement.py
@@ -0,0 +1,21 @@
+# Example 1: Importing a Whole Module
+
+import math
+
+print(math.sqrt(16))
+
+# Example 2: Importing Specific Attributes
+
+from datetime import datetime
+
+print(datetime.now())
+
+# Example 3: Importing with Aliases
+
+import statistics as stats
+
+numbers = [2, 3, 4, 5, 6]
+print(stats.mean(numbers))
+
+# Importing the custom module and using its functions
+# Example 5: Using Built-in Modules for Pratical Tasks
